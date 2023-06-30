17b2fcb28323190d0d7637f154601371f1ae225e
C22 <bhavadipkhunt21@gmail.com>
2023-06-30 11:11:24 +0530

    6

--- app/src/main/java/com/example/login_api/Adapter/User_product_adapter.java
+++ app/src/main/java/com/example/login_api/Adapter/User_product_adapter.java
@@ -1,12 +1,19 @@
 package com.example.login_api.Adapter;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
@@ -14,6 +21,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -21,6 +31,7 @@ import com.example.login_api.DataModels.DeletProduct;
 import com.example.login_api.DataModels.Productdatum;
 import com.example.login_api.DataModels.retro_class;
 import com.example.login_api.R;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 
 import java.util.List;
@@ -33,6 +44,9 @@ public class
 User_product_adapter extends RecyclerView.Adapter<User_product_adapter.Userholder> {
     Context context;
     List<Productdatum> productdata;
+    TextView uname,uprice,udec;
+    Button update;
+    ImageView uimage;
 
     public User_product_adapter(Context context, List<Productdatum> productdata) {
         this.productdata=productdata;
@@ -65,33 +79,80 @@ User_product_adapter extends RecyclerView.Adapter<User_product_adapter.Userholde
                         if (menuItem.getItemId()==R.id.Update)
                         {
 
-                        }
-                        if (menuItem.getItemId()==R.id.delet)
-                        {
-                            retro_class.callapi().DELET_PRODUCT_CALL(Integer.valueOf(productdata.get(holder.getAdapterPosition()).getId())).enqueue(new Callback<DeletProduct>() {
+                            Dialog dialog=new Dialog(context);
+                            dialog.setContentView(R.layout.update_layout);
+                            uname=dialog.findViewById(R.id.update_product_name);
+                            udec=dialog.findViewById(R.id.update_product_desc);
+                            uprice=dialog.findViewById(R.id.update_product_price);
+                            update=dialog.findViewById(R.id.update_button);
+                            uimage=dialog.findViewById(R.id.update_imageview);
+                            uname.setText(productdata.get(holder.getAdapterPosition()).getPname());
+                            uprice.setText(productdata.get(holder.getAdapterPosition()).getPrice());
+                            udec.setText(productdata.get(holder.getAdapterPosition()).getDescription());
+                            uimage.setOnClickListener(new View.OnClickListener() {
                                 @Override
-                                public void onResponse(Call<DeletProduct> call, Response<DeletProduct> response) {
-                                    if (response.body().getConnection()==1)
-                                    {
-                                        if (response.body().getResult()==1)
-                                        {
-                                            Toast.makeText(context, "Product Deleted", Toast.LENGTH_LONG).show();
-                                        }
-                                        else {
-                                            Toast.makeText(context, "Product Not Deleted", Toast.LENGTH_LONG).show();
-                                        }
-                                    }
-                                    else
-                                    {
-                                        Toast.makeText(context, "Somthin Went Wrong", Toast.LENGTH_LONG).show();
-                                    }
+                                public void onClick(View view) {
+                                    CropImage.activity()
+                                            .start(context,new Fragment());
                                 }
+                            });
 
+                            update.setOnClickListener(new View.OnClickListener() {
                                 @Override
-                                public void onFailure(Call<DeletProduct> call, Throwable t) {
+                                public void onClick(View view) {
+                                    String name,price,dec,imagename;
+                                    int id= Integer.parseInt(productdata.get(holder.getAdapterPosition()).getId());
+                                    imagename=productdata.get(holder.getAdapterPosition()).getImage();
+                                    name=uname.getText().toString();
+                                    price=uprice.getText().toString();
+                                    dec=udec.getText().toString();
 
                                 }
                             });
+                        }
+                        if (menuItem.getItemId()==R.id.delet)
+                        {
+                            AlertDialog.Builder builder=new AlertDialog.Builder(context);
+                            builder.setTitle("DELETE...");
+                            builder.setMessage("Are You Sure?");
+                            builder.setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    retro_class.callapi().DELET_PRODUCT_CALL(Integer.valueOf(productdata.get(holder.getAdapterPosition()).getId())).enqueue(new Callback<DeletProduct>() {
+                                        @Override
+                                        public void onResponse(Call<DeletProduct> call, Response<DeletProduct> response) {
+                                            if (response.body().getConnection()==1)
+                                            {
+                                                if (response.body().getResult()==1)
+                                                {
+                                                    Toast.makeText(context, "Product Deleted", Toast.LENGTH_LONG).show();
+                                                }
+                                                else {
+                                                    Toast.makeText(context, "Product Not Deleted", Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                            else
+                                            {
+                                                Toast.makeText(context, "Somthin Went Wrong", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onFailure(Call<DeletProduct> call, Throwable t) {
+
+                                        }
+                                    });
+                                    dialogInterface.dismiss();
+                                }
+
+                            });
+                            builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+
                         }
                         return true;
                     }
@@ -109,6 +170,7 @@ User_product_adapter extends RecyclerView.Adapter<User_product_adapter.Userholde
         return productdata.size();
     }
 
+
     public class Userholder extends RecyclerView.ViewHolder {
         TextView pname,pdec,price;
         ImageView imageView;
@@ -122,4 +184,5 @@ User_product_adapter extends RecyclerView.Adapter<User_product_adapter.Userholde
             relativeLayout=itemView.findViewById(R.id.Reletiv_show);
         }
     }
+
 }
